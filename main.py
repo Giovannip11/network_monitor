24679a913c3773f078a07e034ceb297bcc5366be
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2026-05-29 14:27:42 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,99 @@
 from scanner import scan_network
-from storage import save_devices,load_services
+from storage import save_devices, load_services
 from monitor import compare_devices
-import time
+
 from dotenv import load_dotenv
+from pdf_report import generate_pdf
+
 import os
+import time
+
 load_dotenv()
+
 NET = os.getenv("NETWORK")
-scan_interval = 60
+SCAN_INTERVAL = 60
+
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
 
 def main():
-    
+
     print("Monitor de rede iniciando...")
+
     while True:
-        antigos=load_services()
-        dispositivos = scan_network(NET) 
-        save_devices(dispositivos)  
-        novos,removidos =compare_devices(
+
+        
+
+        antigos = load_services()
+
+        inicio = time.time()
+
+        dispositivos = scan_network(NET)
+
+        fim = time.time()
+        
+        save_devices(dispositivos)
+        
+        clear()
+        
+        
+
+        novos, removidos = compare_devices(
             antigos,
             dispositivos
-        ) 
-        print("\n===DISPOSITIVOS ONLINE===\n")
+        )
+
+        print("=" * 60)
+        print("DISPOSITIVOS ONLINE")
+        print("=" * 60)
+
         for d in dispositivos:
+
+            ip = d.get("ip", "N/A")
+            hostname = d.get("hostname", "Desconhecido")
+            mac = d.get("mac", "N/A")
+            vendor = d.get("vendor", "Desconhecido")
+            os_name = d.get("os", "Desconhecido")
+
             print(
-                f"{d['ip']} |"
-                f"{d['vendor']} |"
-                f"{d['os']}"
+                f"""
+IP: {ip}
+HOST: {hostname}
+MAC: {mac}
+FABRICANTE: {vendor}
+SO: {os_name}
+{"-" * 60}
+                """
             )
-        print("===EVENTOS===")
+
+        print("\nEVENTOS")
+        print("=" * 60)
+
         if novos:
             for ip in novos:
-                print(f"[NOVO] {ip}")
-        
+                print(f"[NOVO DISPOSITIVO] {ip}")
+
         if removidos:
             for ip in removidos:
-                print(f"[NOVO] {ip}")
+                print(f"[DISPOSITIVO OFFLINE] {ip}")
+
         if not novos and not removidos:
             print("Nenhuma mudança.")
-            
-        print(f"\nPróximo scan em {scan_interval}s...\n")
-        time.sleep(scan_interval)
+
+        print("\nINFO")
+        print("=" * 60)
+        print(f"Dispositivos encontrados: {len(dispositivos)}")
+        print(f"Tempo do scan: {fim - inicio:.2f}s")
+        print(f"Próximo scan em {SCAN_INTERVAL}s")
+
+        generate_pdf()
+        
+        time.sleep(SCAN_INTERVAL)
+        
+        
+
+
 if __name__ == "__main__":
     main()
-    
